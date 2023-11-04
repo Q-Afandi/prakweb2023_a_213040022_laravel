26d6f7b7d56a533edbc9801484aff5a378838bad
Q-Afandi <muhammadafandihidayat2003@gmail.com>
2023-11-05 01:07:57 +0700

    Pertemuan 4

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,17 +14,149 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Osas",
         "email" => "Osas@gmail.com",
         "image" => "osas.jpg"
     ]);
 });
 
+
+
+
+
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "judul Post Pertamaa",
+            "slug" => "judul-post-pertama",
+            "author" => "Osas",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Enim corrupti optio voluptate voluptates est culpa quia 
+            eum nihil nisi aut error nobis odio dolorum accusantium ad, 
+            sapiente expedita aliquid cupiditate.Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Enim corrupti optio voluptate voluptates est culpa quia 
+            eum nihil nisi aut error nobis odio dolorum accusantium ad, 
+            sapiente expedita aliquid cupiditate.Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Enim corrupti optio voluptate voluptates est culpa quia 
+            eum nihil nisi aut error nobis odio dolorum accusantium ad, 
+            sapiente expedita aliquid cupiditate.Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Enim corrupti optio voluptate voluptates est culpa quia 
+            eum nihil nisi aut error nobis odio dolorum accusantium ad, 
+            sapiente expedita aliquid cupiditate."
+        ],
+        [
+            "title" => "judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "jajang",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Enim corrupti optio voluptate voluptates est culpa quia 
+            eum nihil nisi aut error nobis odio dolorum accusantium ad, 
+            sapiente expedita aliquid cupiditate.Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Enim corrupti optio voluptate voluptates est culpa quia 
+            eum nihil nisi aut error nobis odio dolorum accusantium ad, 
+            sapiente expedita aliquid cupiditate.Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Enim corrupti optio voluptate voluptates est culpa quia 
+            eum nihil nisi aut error nobis odio dolorum accusantium ad, 
+            sapiente expedita aliquid cupiditate.Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Enim corrupti optio voluptate voluptates est culpa quia 
+            eum nihil nisi aut error nobis odio dolorum accusantium ad, 
+            sapiente expedita aliquid cupiditate.Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Enim corrupti optio voluptate voluptates est culpa quia 
+            eum nihil nisi aut error nobis odio dolorum accusantium ad, 
+            sapiente expedita aliquid cupiditate.Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Enim corrupti optio voluptate voluptates est culpa quia 
+            eum nihil nisi aut error nobis odio dolorum accusantium ad, 
+            sapiente expedita aliquid cupiditate.Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Enim corrupti optio voluptate voluptates est culpa quia 
+            eum nihil nisi aut error nobis odio dolorum accusantium ad, 
+            sapiente expedita aliquid cupiditate.Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Enim corrupti optio voluptate voluptates est culpa quia 
+            eum nihil nisi aut error nobis odio dolorum accusantium ad, 
+            sapiente expedita aliquid cupiditate."
+        ]
+    ];
+
+
+
+    return view('posts', [
+
+        
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+
+//halaman single post
+Route::get('posts/{slug}', function($slug) {
+
+    $blog_posts = [
+        [
+            "title" => "judul Post Pertamaa",
+            "slug" => "judul-post-pertama",
+            "author" => "Osas",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Enim corrupti optio voluptate voluptates est culpa quia 
+            eum nihil nisi aut error nobis odio dolorum accusantium ad, 
+            sapiente expedita aliquid cupiditate.Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Enim corrupti optio voluptate voluptates est culpa quia 
+            eum nihil nisi aut error nobis odio dolorum accusantium ad, 
+            sapiente expedita aliquid cupiditate.Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Enim corrupti optio voluptate voluptates est culpa quia 
+            eum nihil nisi aut error nobis odio dolorum accusantium ad, 
+            sapiente expedita aliquid cupiditate.Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Enim corrupti optio voluptate voluptates est culpa quia 
+            eum nihil nisi aut error nobis odio dolorum accusantium ad, 
+            sapiente expedita aliquid cupiditate."
+        ],
+        [
+            "title" => "judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "jajang",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Enim corrupti optio voluptate voluptates est culpa quia 
+            eum nihil nisi aut error nobis odio dolorum accusantium ad, 
+            sapiente expedita aliquid cupiditate.Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Enim corrupti optio voluptate voluptates est culpa quia 
+            eum nihil nisi aut error nobis odio dolorum accusantium ad, 
+            sapiente expedita aliquid cupiditate.Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Enim corrupti optio voluptate voluptates est culpa quia 
+            eum nihil nisi aut error nobis odio dolorum accusantium ad, 
+            sapiente expedita aliquid cupiditate.Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Enim corrupti optio voluptate voluptates est culpa quia 
+            eum nihil nisi aut error nobis odio dolorum accusantium ad, 
+            sapiente expedita aliquid cupiditate.Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Enim corrupti optio voluptate voluptates est culpa quia 
+            eum nihil nisi aut error nobis odio dolorum accusantium ad, 
+            sapiente expedita aliquid cupiditate.Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Enim corrupti optio voluptate voluptates est culpa quia 
+            eum nihil nisi aut error nobis odio dolorum accusantium ad, 
+            sapiente expedita aliquid cupiditate.Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Enim corrupti optio voluptate voluptates est culpa quia 
+            eum nihil nisi aut error nobis odio dolorum accusantium ad, 
+            sapiente expedita aliquid cupiditate.Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Enim corrupti optio voluptate voluptates est culpa quia 
+            eum nihil nisi aut error nobis odio dolorum accusantium ad, 
+            sapiente expedita aliquid cupiditate."
+        ]
+    ];
+$new_post = [];
+foreach($blog_posts as $post) {
+    if($post["slug"] === $slug) {
+        $new_post = $post;
+    }
+}
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
