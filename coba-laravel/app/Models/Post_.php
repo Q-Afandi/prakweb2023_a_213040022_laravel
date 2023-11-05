2d22f5936c5228cdc9463fe2a6134f8da2dbaab7
Q-Afandi <muhammadafandihidayat2003@gmail.com>
2023-11-05 17:52:37 +0700

    update syntac pertemuan 6

--- coba-laravel/app/Models/Post_.php
+++ coba-laravel/app/Models/Post_.php
@@ -72,3 +72,26 @@ class Post
         
 
 }
+
+// Update Pertemuan 6
+// Syntac 
+// php artisan serve (untuk membuka local host)
+// php artisan migrate:fresh (gunakan syntac tersebut setiap kali kita sudah menambahkan table atau melakukan sesuatu terhadap table)
+// php artisan migrate (digunakan ketika untuk mengupload table yang sudah diubat atau ditambahkan)
+// php artisan migrate:rollback (digunakan untuk menghapus table yang sudah diupload)
+// php artisan tinker (untuk masuk dan mengatur database dari terminal)
+// $user = new App\Models\User; (untuk masuk ke file User agar bisa mengedit apa saja yang akan diisikan kedalam table2 di table user)
+// $user->save() (untuk menyimpan data yang telah dimasukan kedalam table)
+// $user->all() (untuk melihat ada data apasaja yang ada ditable tersebut)
+// $user->first() (untuk melihat data pertama yang ada di table)
+// $user->find(2) (kolom tersebut diisi untuk melihat data yang keberapa atau id yang keberapa yang ingin dilihat datanya)
+// $user->findOrFail() (untuk melihat apkah data tersebut ada jika tidak ada maka akan diberitahu dimana letak kesalahannya atau errornya)
+// exit (untuk keluar dari php artisan tinker)
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
