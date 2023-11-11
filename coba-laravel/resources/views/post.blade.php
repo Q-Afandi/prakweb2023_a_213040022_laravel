0e0fc2a1f78532932c12de61a051293a1d1ca8c4
Q-Afandi <muhammadafandihidayat2003@gmail.com>
2023-11-12 00:02:27 +0700

    Pertemuan 9

--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -4,13 +4,22 @@
 
     <h2 class="mb-5">{{ $post->title }}</h2>
 
-    <p>By, Osas in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
-
-   { !! $post->body !! }
-
-    <a href="/blog" > Back to Post</a>
-
-@endsection
+    @foreach ($posts as $post)
+    <article class="mb-5 border-bottom pb-4">
+        <h2>
+            <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
+        </h2>
+        <p>
+            By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a>
+        </p>
+        <p>{{ $post->excerpt }}</p>
+    
+        <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read more..</a>
+    </article>
+    @endforeach
+    
+    
+    @endsection
 
 
 
