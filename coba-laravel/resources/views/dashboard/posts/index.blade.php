faf84c32ba76a23b0da9bb275ecdb034f6bfc721
Q-Afandi <muhammadafandihidayat2003@gmail.com>
2023-11-26 12:37:27 +0700

    Pertemuan 19

--- coba-laravel/resources/views/dashboard/posts/index.blade.php
+++ coba-laravel/resources/views/dashboard/posts/index.blade.php
@@ -2,6 +2,12 @@
 
 @section('container')
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+  @if(session()->has('success'))
+  <div class="alert alert-success" role="alert">
+    {{ session('success') }}
+  </div>
+@endif
+  
   <h1 class="h2">My Posts</h1>
 </div>
 <div class="table-responsive col-lg-8">
